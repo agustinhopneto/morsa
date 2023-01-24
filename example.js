53d229baf7adb4c7bc6aa3c0f7f46fd41cc64624
Agustinho Neto <agustinho.pneto@gmail.com>
2023-01-24 03:19:43 -0300

    ⭐ feat(example): add example.js

---
+++ example.js
@@ -0,0 +1,17 @@
+const Morsa = require('./lib').default;
+
+const morsa = new Morsa();
+
+const textEncoded =
+  '-.. . -.-. --- -.. . -.. / -... -.-- / -- --- .-. ... .- -.-.--';
+const textDecodedByMorsa = morsa.decode(textEncoded);
+
+const textInput = 'This is MORSA!';
+const textEncodedByMorsa = morsa.encode(textInput);
+
+console.log({
+  textEncoded,
+  textDecodedByMorsa,
+  textInput,
+  textEncodedByMorsa,
+});
